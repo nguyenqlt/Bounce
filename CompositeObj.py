eee24a86c7c8d8d54062106825ea6e194f3a9229
nguyenqlt <nguyenqlt@yahoo.com>
2015-09-24 22:25:15 -0500

    Bouncing Composite Objects
    Bouncing Composite objects with various different bouncing codes

--- CompositeObj.py
+++ CompositeObj.py
@@ -13,11 +13,11 @@ y_max = 3 ## initial max height
 ##create a composite object with f
 f = frame()
 ## QUADCOPTER
-ellipsoid(frame= f, pos=(0,h,0), size=(3, 1, 5))
-ellipsoid(frame= f, pos=(1,h,0), size = (1.5, 0.5, 2.5), color = color.red)
-ellipsoid(frame= f, pos=(1,h,1), size = (1.5, 0.5, 2.5), color = color.red)
-ellipsoid(frame= f, pos=(-1,h,-1), size = (1.5, 0.5, 2.5), color = color.red)
-ellipsoid(frame= f, pos=(-1,h,1), size = (1.5, 0.5, 2.5), color = color.red)
+body=ellipsoid(frame= f, pos=(0,h,0), size=(3, 1, 5))
+part1=ellipsoid(frame= f, pos=(1,h,0), size = (1.5, 0.5, 2.5), color = color.red)
+part2=ellipsoid(frame= f, pos=(1,h,1), size = (1.5, 0.5, 2.5), color = color.red)
+part3=ellipsoid(frame= f, pos=(-1,h,-1), size = (1.5, 0.5, 2.5), color = color.red)
+part4=ellipsoid(frame= f, pos=(-1,h,1), size = (1.5, 0.5, 2.5), color = color.red)
 
 ## DEFINE SPRING
 return_axis = vector(r0*math.cos(theta0),r0*math.sin(theta0),0)
--- TestBouncingQuad.py
+++ TestBouncingQuad.py
@@ -1,15 +1,15 @@
 from visual import *
 
 floor = box(size = (20,2,20),color=(0.6,0.3,0.9))
-#create a composite object with f
-f = frame()
-## QUADCOPTER
-ellipsoid(frame= f, pos=(0,40,0), size=(3, 1, 5))
-ellipsoid(frame= f, pos=(1,40,0), size = (1.5, 0.5, 2.5), color = color.red)
-ellipsoid(frame= f, pos=(1,40,1), size = (1.5, 0.5, 2.5), color = color.red)
-ellipsoid(frame= f, pos=(-1,40,-1), size = (1.5, 0.5, 2.5), color = color.red)
-ellipsoid(frame= f, pos=(-1,40,1), size = (1.5, 0.5, 2.5), color = color.red)
 
+#create a composite object with f
+ball = frame()
+## QUADCOPTER
+body = ellipsoid(frame= ball, pos=(0,50,0), size=(3, 2, 5))
+part1=ellipsoid(frame= ball, pos=(1,50,0), size = (1.5, 0.5, 2.5), color = color.red)
+part2=ellipsoid(frame= ball, pos=(1,50,1), size = (1.5, 0.5, 2.5), color = color.red)
+part3=ellipsoid(frame= ball, pos=(-1,50,-1), size = (1.5, 0.5, 2.5), color = color.red)
+part4=ellipsoid(frame= ball, pos=(-1,50,1), size = (1.5, 0.5, 2.5), color = color.red)
 
 g = 9.8
 finished = False
@@ -17,12 +17,14 @@ mass=15
 velocity = vector(0,0,0)
 dt = 0.01
 
-for obj in f.objects:
+
 while not finished:
     rate(100)
     force=vector(0,-mass*g,0)
     acceleration=force/mass
     velocity +=acceleration*dt
-    f.pos += velocity*dt - 0.5*acceleration*dt**2
-    if f.pos.y < mass*0.5*floor.height:
+    ball.pos += velocity*dt - 0.5*acceleration*dt**2
+	##does not work with body.height... works with negative and body.pos.y
+    if ball.pos.y < -(body.pos.y+0.5*floor.height):
         velocity.y = -velocity.y
+	print "this happened"
